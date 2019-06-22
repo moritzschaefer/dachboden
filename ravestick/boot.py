4154f9ed29e28767e7e2823448890cb4d4b9d912
Marco <mmorik@yahoo.de>
2019-06-22 12:14:53 -0400

    new main and workable boot

--- ravestick/boot.py
+++ ravestick/boot.py
@@ -1,17 +1,31 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import network
-import webrepl
-import time
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('Incubator', 'Fl4mongo')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
 
+do_connect()
+import webrepl
+"""
 sta_if = network.WLAN(network.STA_IF)
+
+ap_if =network.WLAN(network.AP_IF)
+ap_if.active(True)
+
 x = 0
 while not sta_if.isconnected() and x < 5:
     sta_if.active(True)
     sta_if.connect('Incubator', 'Fl4mongo')
     time.sleep(1)
     x += 1
-
+"""
 
 webrepl.start()
--- ravestick/main.py
+++ ravestick/main.py
@@ -1,6 +1,15 @@
 import machine
 import neopixel
 import time
+#Auge1 0-11 Auge2 12-23 Kiemen1 24-x Kiemen2 x+1-y Inner y+1-max
+
+class Eye:
+    def __init__(self, pixels):
+        self.pixels = pixels
+
+    def rolling_eye(self, np):
+        pass
+
 
 
 def cycle(np, pixel_count):
@@ -23,6 +32,21 @@ def bounce(np, pixel_count):
         np.write()
         time.sleep(0.60)
 
+def all_switching(np, pixel_count):
+    for j in range(pixel_count):
+        np[j] = (0, 0, 0)
+    for i in range(0, pixel_count,2):
+        np[i % pixel_count] = (2, 4, 8)
+    np.write()
+    time.sleep(0.25)
+
+    for j in range(pixel_count):
+        np[j] = (0, 0, 0)
+    for i in range(1, pixel_count,2):
+        np[i % pixel_count] = (8, 4, 2)
+    np.write()
+    time.sleep(0.25)
+
 
 def fade_in_out(np, pixel_count):
     for i in range(0, 4 * 256, 8):
@@ -42,11 +66,12 @@ def clear(np, pixel_count):
 
 
 def main():
-    pixel_count = 80
+    pixel_count = 180
     np = neopixel.NeoPixel(machine.Pin(13), pixel_count)
 
     while True:
-        cycle(np, pixel_count)
+        all_switching(np,pixel_count)
+        #cycle(np, pixel_count)
 
 
 if __name__ == "__main__":
