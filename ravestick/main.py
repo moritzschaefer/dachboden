a484cf1627ba344d80590f8b54c62f313c31c9c0
Marco <marco.t.morik@gmail.com>
2019-06-23 11:00:18 +0200

    working blink eye

--- ravestick/main.py
+++ ravestick/main.py
@@ -1,15 +1,58 @@
 import machine
 import neopixel
-import time
+from utime import ticks_diff, ticks_ms, ticks_add, sleep_ms
 #Auge1 0-11 Auge2 12-23 Kiemen1 24-x Kiemen2 x+1-y Inner y+1-max
 
-class Eye:
-    def __init__(self, pixels):
+PIXEL_COUNT = 180
+np = neopixel.NeoPixel(machine.Pin(13), PIXEL_COUNT)
+
+class Module(object):
+    def __init__(self, np, pixels, color=None):
+        self.np = np
         self.pixels = pixels
+        self.color = color
 
-    def rolling_eye(self, np):
+    def all_pixels(self, onoff=True, color=None):
+        if onoff:
+            if not color:
+                color = self.color
+            if not color:
+                color = (100, 100, 100)
+        else:
+            color = (0, 0, 0)
+        for pixel in self.pixels:
+            np[pixel] = color
+
+class Kieme(Module):
+    '''
+    Only ONE kieme
+    '''
+    def __init__(self, pixels, color):
+        super(Kieme, self).__init__(np, pixels, color)
+        self.all_pixels(True, color) 
+
+    def step(self, ticks):
         pass
+        
 
+
+class Eye(Module):
+    def __init__(self, np, pixels, color):
+        super(Eye, self).__init__(np, pixels, color)
+        self.blink_end = None
+        self.blink_duration = 100
+        self.all_pixels(True, color)
+
+    def blink(self, ticks):
+        print('blink')
+        self.blink_end = ticks_add(ticks, self.blink_duration)
+        self.all_pixels(False)
+
+    def step(self, ticks):
+        if self.blink_end is not None and ticks_diff(self.blink_end, ticks) < 0:
+            print('blink end')
+            self.all_pixels(True)
+            self.blink_end = None
 
 
 def cycle(np, pixel_count):
@@ -66,12 +109,27 @@ def clear(np, pixel_count):
 
 
 def main():
-    pixel_count = 180
-    np = neopixel.NeoPixel(machine.Pin(13), pixel_count)
+    left_eye = Eye(np, list(range(12)), (200, 0, 0))
+    right_eye = Eye(np, list(range(12, 24)), (0, 200, 0))
+    modules = [left_eye, right_eye]
+
+    i = 0
 
     while True:
-        all_switching(np,pixel_count)
-        #cycle(np, pixel_count)
+        ticks = ticks_ms()
+
+        if i % 100 == 0:
+            left_eye.blink(ticks)
+
+        for module in modules:
+            module.step(ticks)
+
+
+        np.write()
+        i += 1
+        sleep_ms(50)
+        #all_switching(np, pixel_count)
+        #cycle(rp, pixel_count)
 
 
 if __name__ == "__main__":
