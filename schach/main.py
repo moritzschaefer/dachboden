1f97a4c0f007823897a39c4d06a7c79b1b4c2ad3
Marco <mmorik@yahoo.de>
2019-08-02 02:32:25 +0200

    TODO für schach!

--- schach/main.py
+++ schach/main.py
@@ -153,3 +153,33 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+#TODO
+#Move async to esp
+#async http soket.
+#IP websocket 192.168.178.4
+# Website:
+
+#  Two time fields as buttons
+#  Send two signals
+#  TODO learn how to display live changing time
+
+
+
+# Modules:
+"""
+DMX Strobo Access (not interupting the time)
+
+Intern Network:
+Time is running down /Different color 
+
+Settings: (Color for each player, Time per match, increment, delay )  
+Each player is slowling losing color for each turn. You see the delay/increment first cycle and then the overall time starts per turn and goes in the right speed. 5 sec left, very fast, 5 min left very slowe
+ 
+
+"""
+
+
+#Sonstiges
+#Milchglas
