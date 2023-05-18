ddce9c5e0db21187d46c0098babc4ca7a47681ef
Marco <m.morik@tu-berlin.de>
2023-05-18 23:05:10 +0200

    simple strobo tester

---
+++ UhrStrobo/main.py
@@ -0,0 +1,34 @@
+import machine, utime
+
+
+PIN_IDS = [2, 4, 12, 14]
+
+PINS = []
+for id in PIN_IDS:
+    pin = machine.Pin(id, machine.Pin.OUT)
+    PINS.append(pin)
+
+def strobo():
+    for pin in PINS:
+        pin.on()
+    utime.sleep_ms(5)
+    for pin in PINS:
+        pin.off()
+
+
+
+
+def main():
+    while True:
+        for j in [5,20,40,100,200,500]:
+            for i in range(10):
+                strobo()
+                utime.sleep_ms(j)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
