9f703738218fa97da9dfc9338f6f3c032bea80d7
Marco <m.morik@tu-berlin.de>
2024-07-15 20:21:43 +0200

    new version of schlauch hero

--- SchlauchHero/main.py
+++ SchlauchHero/main.py
@@ -16,19 +16,18 @@ DATA_PIN = 4
 LED_PINS = [13]
 INPUT_PINS = [5, 16,17,18]
 PINS_SETTING = [21,22,23,25]
-LIFE_PINS = [3, 4]
-SCORE_PINS = [5, 6]
+LIFE_PINS = [25, 26]
+SCORE_PINS = [32, 33]
 THRESHHOLD = 150
 PIXELS = 5*60
 
 class SchlauchHero:
     colors=[(200,0,0), (0,200,0), (0,0,200), (200,0,200), (200,200,200), (0,0,0)]
-    level_speed = [1000, 500, 250, 100, 50, 25, 10]
+    level_speed = [500, 250, 100, 50, 25, 10]
+    ball_speed = 5
     width = 10
     light = 0
     def __init__(self):
-        self.time = utime.ticks_ms()
-        self.button_time = utime.ticks_ms()
         self.int_board = [4 for i in range(PIXELS)]
         self.board = [self.colors[i] for i in self.int_board]
         self.sender = Sender()
@@ -37,7 +36,9 @@ class SchlauchHero:
         self.sender.send(self.board)
         self.restart()
         self.current_colors = [0, 1, 2, 3]
-
+        self.ball_width = 8
+        self.ball_board = [-1 for _ in range(PIXELS)] # Stores color and pos of ball
+        self.change = 0
     def update_score(self):
         self.score_monitor.number(self.score)
 
@@ -53,10 +54,14 @@ class SchlauchHero:
     def restart(self):
         self.mode = "live"
         self.time_progress = 0
-        self.life = 3
+        self.life = 5
         self.score = 0
         self.cur_generation_width = 0
         self.cur_generation_color = 4
+
+        self.time = utime.ticks_ms()
+        self.button_time = utime.ticks_ms()
+        self.ball_time = utime.ticks_ms()
         self.update_score()
         self.update_life()
 
@@ -88,49 +93,104 @@ class SchlauchHero:
                 self.int_board[self.light] = 4 # Some reward
                 self.score += 1
                 self.update_score()
+                self.point_sequence(pressed[0])
             else:
                 # Wrong
                 self.life -= 1
+                self.point_sequence(-1)
                 if self.life <= 0:
+                    self.loose_sequence()
                     self.restart()
 
+
+    def loose_sequence(self):
+        # Let all lights strobe
+        for i in range(PIXELS):
+            self.ball_board[i] = -1
+            self.int_board[i] = -1
+        for _ in range(5):
+            for i in range(PIXELS):
+                self.board[i] = self.colors[-2]
+            self.sender.send(self.board)
+            utime.sleep_ms(25)
+            for i in range(PIXELS):
+                self.board[i] = self.colors[-1]
+            self.sender.send(self.board)
+            utime.sleep_ms(25)
+
+    def point_sequence(self, color):
+        # Send the color as a ball in opposite direction
+        for i in range(self.ball_width):
+            self.ball_board[i] = color
+
+    def merge_ball_and_board(self):
+        for i in range(PIXELS):
+            if self.ball_board[i] >= 0:
+                self.board[i] = self.colors[self.ball_board[i]]
+            else:
+                self.board[i] = self.colors[self.int_board[i]]
+
+    def move_ball(self):
+        for i in range(1, PIXELS):
+            self.ball_board[PIXELS - i] = self.ball_board[PIXELS - i - 1]
+        self.ball_board[0] = -1
     def move_color(self):
         # Move the light, randomly spawn a new color
         for i in range(PIXELS-1):
             self.int_board[i] = self.int_board[i+1]
-            self.board[i] = self.board[i+1]
+            #self.board[i] = self.colors[self.int_board[i]]
 
         if self.cur_generation_width <= 0:
-            if len(self.current_colors) == 0:
-                self.cur_generation_color = randint(0,4)
-            self.cur_generation_color = self.current_colors[randint(0,len(self.current_colors))]
-            self.cur_generation_width = self.width
-
+            if randint(0,100) > 25:
+                if len(self.current_colors) == 0:
+                    self.cur_generation_color = randint(0,4)
+                else:
+                    self.cur_generation_color = self.current_colors[randint(0,len(self.current_colors))]
+                self.cur_generation_width = self.width
+            else:
+                # Generate some dark pixels
+                self.cur_generation_color = self.colors[-1]
+                self.cur_generation_width = self.width // 2
         self.int_board[PIXELS-1] = self.cur_generation_color
-        self.board[PIXELS-1] = self.colors[self.cur_generation_color]
+        #self.board[PIXELS-1] = self.colors[self.cur_generation_color]
         self.cur_generation_width -= 1
 
     def send_color(self, command):
-        for i in range(1, PIXELS):
-            self.int_board[i] = self.int_board[i-1]
-            self.board[i] = self.board[i-1]
         pressed = [i for i in range(4) if command[i]]
+
         if len(pressed) == 1:
+            for i in range(1, PIXELS):
+                self.int_board[i] = self.int_board[i - 1]
+                self.board[i] = self.colors[self.int_board[i]]
+
             self.int_board[0] = pressed[0]
             self.board[0] = self.colors[pressed[0]]
 
     def step(self, command):
-        if self.mode =="sending":
-            self.send_color(command)
-            return
 
-        if utime.ticks_ms() - self.button_time > 50:
-            self.check_correct(command)
-        # Check if time is ready to move the light
-        if utime.ticks_ms() - self.time > self.level_speed[max(self.score//10,len(self.level_speed)+1)]:
-            self.time = utime.ticks_ms()
-            self.move_color()
+        if self.mode == "sending":
+            self.send_color(command)
+            self.change = 1
+        else:
+            if utime.ticks_ms() - self.button_time > 50:
+                self.check_correct(command)
+
+            # Check if time is ready to move the light
+            if utime.ticks_ms() - self.time > self.level_speed[max(self.score//10,len(self.level_speed)+1)]:
+                self.time = utime.ticks_ms()
+                self.move_color()
+                self.change = 1
+
+            if utime.ticks_ms() - self.ball_time > self.ball_speed:
+                self.ball_time = utime.ticks_ms()
+                self.move_ball()
+                self.change = 1
+
+        if self.change:
+            self.merge_ball_and_board()
             self.sender.send(self.board)
+            self.change=0
+
 
     def set_colors(self, colors):
         self.current_colors = colors
@@ -185,6 +245,7 @@ async def main(hero, controller, settings):
     i = 0
     while True:
         command = controller.step()
+        print("We observed this command: ", command)
         #print(TouchCommand)
         hero.step(command)
         await asyncio.sleep_ms(5)
